54945518d3191ff9baebc1f96b5503d9ad0ac58f
Ярослав Бессарабов <bessarabovaroslav801@gmail.com>
2026-09-11 11:35:02 +0300

    Lab02:string to number conversion, Parse vs TryParse

--- Program.cs
+++ Program.cs
@@ -46,15 +46,38 @@
 
 // System.Console.WriteLine($"{fullName}, возраст {studentAge}, средний балл {gpa}");
 
-using System.Runtime.Intrinsics.Arm;
+// using System.Runtime.Intrinsics.Arm;
 
-System.Console.WriteLine();
-System.Console.WriteLine("Ввод теста");
+// System.Console.WriteLine();
+// System.Console.WriteLine("Ввод теста");
 
-System.Console.Write("Введите ваше имя: ");
-string enteredName = System.Console.ReadLine();
+// System.Console.Write("Введите ваше имя: ");
+// string enteredName = System.Console.ReadLine();
 
-System.Console.Write("Введите название вашей группы: ");
-string enteredGroup = System.Console.ReadLine();
+// System.Console.Write("Введите название вашей группы: ");
+// string enteredGroup = System.Console.ReadLine();
 
-System.Console.WriteLine($"Здравствуйте, {enteredName} из группы {enteredGroup}!");
+// System.Console.WriteLine($"Здравствуйте, {enteredName} из группы {enteredGroup}!");
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("Ввод чисел: Convert и Parse");
+
+// System.Console.Write("Введите ваш год рождения: ");
+// string birthYearInput = System.Console.ReadLine();
+// int birthYearConvert = Convert.ToInt32(birthYearInput);
+// int birthYearParse = int.Parse(birthYearInput);
+
+// System.Console.WriteLine($"Convert.ToInt32: {birthYearConvert}");
+// System.Console.WriteLine($"int.Parse: {birthYearParse}");
+// System.Console.WriteLine($"В 2030 вам будет: {2030 - birthYearConvert} лет");
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("Ввод чисел: TryParse");
+
+// System.Console.Write("Введите количество прочитанных книг за семестр: ");
+// string booksInput = Console.ReadLine();
+
+// bool wasSuccessful = int.TryParse(booksInput, out int booksCount);
+
+// System.Console.WriteLine($"Удалось преобразовать: {wasSuccessful}");
+// System.Console.WriteLine($"Значение переменной booksCount: {booksCount}");
--- obj/Debug/net10.0/Lab02_Bessrabov.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02_Bessrabov.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02_Bessrabov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df417fe2b93f96fd72bab7ac71be19667f2f6419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391d5fb274989c02ec13aedf71082c227420d567")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02_Bessrabov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02_Bessrabov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
