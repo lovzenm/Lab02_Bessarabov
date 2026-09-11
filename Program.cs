99c43c3b1d02c7c5793ca060dbec25f0e45a22be
Ярослав Бессарабов <bessarabovaroslav801@gmail.com>
2026-09-11 11:03:06 +0300

    Lab02: char and decimal precision

--- Program.cs
+++ Program.cs
@@ -16,3 +16,23 @@
 // byte maxByte = 255;
 // byte overflowed = (byte)(maxByte + 1);
 // System.Console.WriteLine($"255 + 1 для byte = {overflowed}");
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("char");
+
+// char firstLetter = 'A';
+// char separator = '-';
+// int charAsNumber = firstLetter;
+// System.Console.WriteLine($"Символ: {firstLetter}, разделитель: {separator}");
+// System.Console.WriteLine($"Код символа '{firstLetter}' в Unicode: {charAsNumber}");
+// System.Console.WriteLine($"Табуляция:\tпосле таба");
+// System.Console.WriteLine($"Перенос:\nпосле переноса");
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("decimal против double");
+
+// double priceDouble = 0.1 + 0.2;
+// decimal priceDecimal = 0.1m + 0.2m;
+
+// System.Console.WriteLine($"double: 0.1 + 0.2 = {priceDouble}");
+// System.Console.WriteLine($"decimal: 0.1 + 0.2 = {priceDecimal}");
